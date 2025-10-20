17b044d988d74208e4da9c27e54af8ccd777ed69
Darnell <darnellgarcia26@gmail.com>
2025-10-20 19:10:54 -0400

    Implemented Basic JWT Auth

--- App/views/staff.py
+++ App/views/staff.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, jsonify, request, send_from_directory, flash, redirect, url_for
 from flask_jwt_extended import jwt_required, current_user as jwt_current_user
-
 from.index import index_views
+from App.utils.user_type_decorator import require_jwt_role
 
 from App.controllers import (
     view_all_internships,
@@ -11,11 +11,13 @@ from App.controllers import (
 staff_views = Blueprint('staff_views', __name__, template_folder='../templates')
 
 @staff_views.route('/<string:staff_id>/list-internships', methods=['GET'])
+@require_jwt_role('staff')
 def list_internships(staff_id):
     internships = view_all_internships()
     return jsonify(internships), 200
 
 @staff_views.route('/<string:staff_id>/add-student', methods=['POST'])
+@require_jwt_role('staff')
 def add_student(staff_id):
     data = request.get_json()
     student_id = data.get('student_id')
